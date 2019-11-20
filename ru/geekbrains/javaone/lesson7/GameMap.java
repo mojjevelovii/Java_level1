76e3a19951b424bbccef719cbb418c70a1d01ed7
Veronika Shumilova <mojjevelovii@gmail.com>
2019-11-20 22:36:09 +0300

    Добавила сетку с кнопками.

--- ru/geekbrains/javaone/lesson7/GameMap.java
+++ ru/geekbrains/javaone/lesson7/GameMap.java
@@ -16,11 +16,34 @@ import java.awt.*;
 public class GameMap extends JPanel {
     public static final int GM_HVH = 0;
     public static final int GM_HVA = 1;
+    private static final int GAP = 3;
+
     GameMap() {
         setBackground(Color.BLACK);
     }
 
     void startNewGame(int mode, int fieldSizeX, int fieldSizeY, int winLength) {
         System.out.printf("mode=%d, fieldSize=%d, winLen=%d\n", mode, fieldSizeX, winLength);
+
+        initLayout(fieldSizeX, fieldSizeY);
+
+        fillField(fieldSizeX, fieldSizeY);
+
+        this.updateUI();
     }
+
+    private void initLayout(int fieldSizeX, int fieldSizeY) {
+        GridLayout grid = new GridLayout(fieldSizeX, fieldSizeY, GAP, GAP);
+        grid.preferredLayoutSize(this);
+        this.setLayout(grid);
+    }
+
+    private void fillField(int fieldSizeX, int fieldSizeY) {
+        int buttonQuantity = fieldSizeX * fieldSizeY;
+        for (int i = 0; i < buttonQuantity; i++) {
+            this.add(new JButton());
+        }
+    }
+
+
 }
